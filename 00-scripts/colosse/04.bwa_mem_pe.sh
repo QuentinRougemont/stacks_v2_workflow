8115dfc13be71247429e91526249975fdf835d50
ubuntu <quentinrougemont@orange.fr>
2019-04-17 04:14:36 -0400

    add cluster scripts

--- 00-scripts/colosse/04.bwa_mem_pe.sh
+++ 00-scripts/colosse/04.bwa_mem_pe.sh
@@ -8,4 +8,8 @@
 #PBS -M quentinrougemont@orange.fr
 ##PBS -m ea 
 #
+source /clumeq/bin/enable_cc_cvmfs
+module load samtools/1.5
+module load bwa/0.7.15
+
 ./00-scripts/04.bwa_mem_align_reads_pe.sh
